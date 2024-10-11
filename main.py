4339e8aab755324686866a4efffd1394278af954
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 20:07:10 +0500

    final commit

--- main.py
+++ main.py
@@ -27,23 +27,24 @@ scissors = '''
 ---.__(___)
 '''
 
-# list = [[rock], [paper], [scissors]]
+images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
-print(f"You chose: {user_choice}")
-computer_choice = random.randint(0,2)
-print(f"Computer chose: {computer_choice}")
-
 if user_choice >=3 or user_choice < 0:
-    print("You typed an invalid number, you lose!") 
-elif user_choice == 0 & computer_choice == 2:
-    print("You win!")
-elif user_choice == 2 & computer_choice == 0:
-    print("You lose")    
-elif user_choice < computer_choice:
-    print("You lose")    
-elif user_choice > computer_choice:
-    print("You win!")    
-elif user_choice == computer_choice:
-    print("It's a draw!")    
+    print("You typed an invalid number, you lose!")
+else:    
+    print(f"You chose: {images[user_choice]}")        
+    computer_choice = random.randint(0,2)
+    print(f"Computer chose: {images[computer_choice]}")
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose")    
+    elif user_choice < computer_choice:
+        print("You lose")    
+    elif user_choice > computer_choice:
+        print("You win!")    
+    elif user_choice == computer_choice:
+        print("It's a draw!")    
      
     
