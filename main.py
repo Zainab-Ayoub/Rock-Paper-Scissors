5e1b6b6ed3e4adc33bbacce33345d0b3b8ba6bfb
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 19:55:06 +0500

    login

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-row1 = ["⬜", "⬜", "⬜",]
-row2 = ["⬜", "⬜", "⬜",]
-row3 = ["⬜", "⬜", "⬜",]
+import random
 
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-row = int(position[0])
-column = int(position[1])
-map[column-1] [row-1] = "X"
-print(f"{row1}\n{row2}\n{row3}")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# list = [[rock], [paper], [scissors]]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+print(f"You chose: {user_choice}")
+computer_choice = random.randint(0,2)
+print(f"Computer chose: {computer_choice}")
+
+if user_choice >=3 or user_choice < 0:
+    print("You typed an invalid number, you lose!") 
+elif user_choice == 0 & computer_choice == 2:
+    print("You win!")
+elif user_choice == 2 & computer_choice == 0:
+    print("You lose")    
+elif user_choice < computer_choice:
+    print("You lose")    
+elif user_choice > computer_choice:
+    print("You win!")    
+elif user_choice == computer_choice:
+    print("It's a draw!")    
+     
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+choice == 0 & computer_choice == 1:
+#     print("You lose")
+# elif user_choice == 1 & computer_choice == 2:
+#     print("You lose")    
+# elif user_choice == 2 & computer_choice == 0:
+#     print("You lose")    
+# elif user_choice == 1 & computer_choice == 0:
+#     print("You win")    
+# elif user_choice == 2 & computer_choice == 1:
+#     print("You win")    
+# else:
+#     print("Draw")      
