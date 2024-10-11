786b32ae4b90a2c08dfaa4ce3e08509c7363a5fa
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 19:08:43 +0500

    new method

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import random
+row1 = ["⬜", "⬜", "⬜",]
+row2 = ["⬜", "⬜", "⬜",]
+row3 = ["⬜", "⬜", "⬜",]
 
-random_float = random.random()
-print(random_float)
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+row = int(position[0])
+column = int(position[1])
+selected_row = map[row-1]
+selected_row [column-1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
