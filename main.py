6c0fe759bac76d40350dc838025e942e1885ddea
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-11 19:15:31 +0500

    easier way

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 row = int(position[0])
 column = int(position[1])
-selected_row = map[row-1]
-selected_row [column-1] = "X"
+map[column-1] [row-1] = "X"
 print(f"{row1}\n{row2}\n{row3}")
